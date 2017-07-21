269e6d46cf2579d8e2ecbc4168bf5c097ca8dde0
Lizabeth Petersen <lizpetersen928@gmail.com>
2017-07-20 19:19:12 -0700

    Create Thursday notes file

--- code/thursday.js
+++ code/thursday.js
@@ -1,9 +1,9 @@
-var day = 'Thursday',
-    thursday = 'thursday';
+var day = 'Thursday';
+var  thursday = 'thursday';
 
 // How many equals signs does it take to muck up your code?
-if (day = 'thursday') {
-  console.log('Test 1: Well lookee there, it\'s ' + day);
+//if (day = 'thursday') {
+// console.log('Test 1: Well lookee there, it\'s ' + day);
 }
 
 // The case of the failing "if" statement
@@ -46,17 +46,31 @@ if (day === 'Thursday') {
 
 var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-// console.log('index 0: ' + days[0]);
-// console.log('index 1: ' + days[1]);
-// console.log('index 2: ' + days[2]);
-// console.log('index 6: ' + days[6]);
-// console.log('index 7: ' + days[7]);
-// console.log('index 20: ' + days[20]);
+console.log('index 0: ' + days[0]);
+console.log('index 1: ' + days[1]);
+console.log('index 2: ' + days[2]);
+console.log('index 3: ' + days[3]);
+console.log('index 4: ' + days[4]);
+console.log('index 5: ' + days[5]);
+console.log('index 6: ' + days[6]);
+
+console.log('index 7: ' + days[7]);
+console.log('index 20: ' + days[20]);
+
 
 // TODO: DRY it up!
-// for (var i = 0; i < 7; i++) {
-//   console.log('index ' + i + ': ' + days[i]);
-// }
+for (var i = 0; i < 7; i++) {
+  console.log('index ' + i + ': ' + days[i]);
+}
+// the variable i = 0 tells us where to start; the middle expression, i < 7 
+// runs the rest of the loop and tells us where to stop;
+// the last expression, i++, tells us to increase by increments of 1.
+
+
+// 0. Write tests. How does it need to work?
+// 1. Make it run .
+// 2. Make it right.
+// 3. Make it fast/pretty/etc...
 
 
 // TODO: Replace the "7" in "i < 7" with the length of the array
